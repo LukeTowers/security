7e4d1dfafa0581320e9508af587f14460c69141c
Alwin Drenth <alwin@vdlp.nl>
2018-01-01 14:15:58 +0100

    Remove unused code

--- classes/eventsubscribers/BackendEventSubscriber.php
+++ classes/eventsubscribers/BackendEventSubscriber.php
@@ -7,8 +7,6 @@ namespace Adrenth\Security\Classes\EventSubscribers;
 use Adrenth\Security\Classes\Contracts\EventSubscriber;
 use Adrenth\Security\Classes\EventListeners\Backend\PageBeforeDisplay;
 use Adrenth\Security\Classes\EventListeners\Backend\FormExtendFields;
-use Adrenth\Security\Classes\EventListeners\Backend\ListExtendColumns;
-use Adrenth\Security\Classes\EventListeners\Backend\ListInjectRowClass;
 use Adrenth\Security\Classes\EventListeners\Backend\UserLogin;
 use October\Rain\Events\Dispatcher;
 
@@ -25,8 +23,6 @@ class BackendEventSubscriber implements EventSubscriber
     public function subscribe(Dispatcher $dispatcher): void
     {
         $dispatcher->listen('backend.form.extendFields', FormExtendFields::class);
-        $dispatcher->listen('backend.list.injectRowClass', ListInjectRowClass::class);
-        $dispatcher->listen('backend.list.extendColumns', ListExtendColumns::class);
         $dispatcher->listen('backend.user.login', UserLogin::class);
         $dispatcher->listen('backend.page.beforeDisplay', PageBeforeDisplay::class);
     }
